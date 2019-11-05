e38bf3cb809677147e51dceca50e33265eaaf722
Rachael Dottle <rachael.dottle@ibm.com>
2019-11-05 16:51:29 +0100

    remove

--- src/js/index.js
+++ src/js/index.js
@@ -15,33 +15,5 @@ const mapContainer = d3.select("#map");
 let newMap = new Map();
 
 
-var clientID = '419164154973-v5rpoogthlb6i121k1cf46m0jjm88nj4.apps.googleusercontent.com';
-var API = 'AIzaSyBtIthLcViQ0uDAbXFaXEyDPS8ht5fSfcE';
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-
-function getData() {
-    gapi.sheets.spreadsheets.values.get({
-      spreadsheetId: '16nH7JsFrUuVLBAwU6bjCuX2GmcnjeYp47Ia7tnEMapw',
-      range: 'Sheet1!A2:F',
-    }).then(function(response) {
-      console.log(response.result.values);
-    });
-  }
-
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
-
-function initClient() {
-  gapi.client.init({
-    apiKey: API,
-    clientId: clientID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    getData();
-  });
-}
 
 handleClientLoad();
