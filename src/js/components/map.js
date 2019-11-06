4708db28ef28b809ec66d01613ac94c31a6553ed
Rachael Dottle <rachael.dottle@ibm.com>
2019-11-06 10:03:46 +0100

    interactive, read data, sashi

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -3,7 +3,8 @@ import { queue } from "d3-queue";
 import { geoAugust } from "d3-geo-projection";
 
 const topojson = require("topojson-client");
-const world = require("./../../data/countries_110.json")
+const world = require("./../../data/countries_110.json");
+const whereBeen = require("./../../data/where.json");
 class Map {
     
     constructor(opts) {
@@ -14,6 +15,7 @@ class Map {
     	this.height = window.innerHeight - 200;
     	this.width = window.innerWidth;
 		this.margin = {top: 0, right: 0, bottom: 0, left: 0};
+		this.cat = "https://img.icons8.com/pastel-glyph/50/000000/cat--v3.png";
 
 		this.path = d3.geoPath();
 
@@ -27,8 +29,8 @@ class Map {
 
 
 		this.projection = geoAugust()
-                   .scale(300)
-                  .translate( [this.width / 2.2, this.height / 1.5]);
+                   .scale(260)
+                  .translate( [this.width / 2.5, this.height / 1.5]);
 
 		this.path = d3.geoPath().projection(this.projection);
 		this.ready()
@@ -36,16 +38,46 @@ class Map {
 
 	ready() {
     
-	  this.svg.append("g")
-	      .attr("class", "countries")
-	    .selectAll("path")
+	  this.g = this.svg.append("g")
+	      .attr("class", "countries");
+
+
+		this.g.on("wheel.zoom",() => {
+		        var currScale = this.projection.scale();
+		        var newScale = currScale - 2*event.deltaY;
+		        var currTranslate = this.projection.translate();
+		        var coords = this.projection.invert([event.offsetX, event.offsetY]);
+		        this.projection.scale(newScale);
+		        var newPos = this.projection(coords);
+		        this.projection.translate([currTranslate[0] + (event.offsetX - newPos[0]), currTranslate[1] + (event.offsetY - newPos[1])]);
+		        this.g.selectAll("path").attr("d", this.path);
+		        this.g.selectAll("circle")
+		        	.attr("cx",  (d) => { return this.projection([d.long,d.lat])[0] })
+					.attr("cy",  (d) => { return this.projection([d.long,d.lat])[1] })
+				this.g.selectAll("image").attr("d", this.path).attr("transform", (d)=> { return "translate(" + this.projection([d.long,d.lat]) + ")"; })
+
+
+		    })
+		    .call(d3.drag().on("drag", () => {
+		        var currTranslate = this.projection.translate();
+		        this.projection.translate([currTranslate[0] + d3.event.dx,
+		                              currTranslate[1] + d3.event.dy]);
+		        this.g.selectAll("path").attr("d", this.path);
+		        this.g.selectAll("circle").attr("cx",  (d) => { return this.projection([d.long,d.lat])[0] })
+					.attr("cy",  (d) => { return this.projection([d.long,d.lat])[1] })
+				this.g.selectAll("image").attr("d", this.path).attr("transform", (d)=> { return "translate(" + this.projection([d.long,d.lat]) + ")"; })
+
+
+		    }));
+
+	 this.g.selectAll("path")
 	      .data(this.countries.features)
 	    .enter().append("path")
 	      .attr("d", this.path)
 	      .style("fill", "#fff")
 	      .style("stroke", "#cdcdcd")
 	 
-	  this.svg.append("g")
+	  this.g.append("g")
 	      .attr("class", "land")
 	    .selectAll("path")
 	      .data(this.land.features)
@@ -55,7 +87,41 @@ class Map {
 	      .style("stroke", "#222222")
 
 
-	}
+		this.g.selectAll("circle")
+			.data(whereBeen.filter(d => {return d.person == "Rachael"})).enter()			
+			.append("circle")
+			.attr("class", d => {
+				console.log(d.person)
+				return d.person == "Sashi" ? "sashi" : "rachael"})
+			.attr("cx",  (d) => { return this.projection([d.long,d.lat])[0] })
+			.attr("cy",  (d) => { return this.projection([d.long,d.lat])[1] })
+			.attr("r", "0px")
+			.style("fill", "#d41367")
+			.style("opacity", "1")		
+			.transition()
+			.ease(d3.easeBounce)
+			.duration(2000)
+			.attr("r", d => {return d.person == "Sashi" ? "10px" : "7px"})
+			.transition()
+			.ease(d3.easeBounce)
+			.duration(2000)
+			.attr("r", d => {return d.person == "Sashi" ? "100px" : "5px"})
+
+		this.g.selectAll(".test")
+			.data(whereBeen.filter(d => {return d.person == "Sashi"})).enter()
+			.append('image')
+			.attr('d', d => {return d.person == "Sashi" ? this.path : null})
+			.attr('r', 1)
+			.attr("transform", (d)=> { return "translate(" + this.projection([d.long,d.lat]) + ")"; })
+	        .attr("xlink:href", this.cat)
+	        .attr("x", (d) => { return -25;})
+	        .attr("y", (d) => { return -25;})
+	        .attr("height", 20)
+	        .attr("width", 20);
+
+
+		}
+
 }
 
 export { Map };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,44 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./client_secret.json');
+var fs = require('fs');
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('16nH7JsFrUuVLBAwU6bjCuX2GmcnjeYp47Ia7tnEMapw');
+
+let spreadsheetData = [];
+
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+    for (var i = rows.length - 1; i >= 0; i--) {
+      spreadsheetData.push({
+        city: rows[i].city,
+        country: rows[i].country,
+        person: rows[i].person,
+        date: rows[i].date,
+        mostrecent: rows[i].mostrecent,
+        image: rows[i].image,
+        lat: rows[i].lat,
+        long: rows[i].long
+      })
+    }
+      
+      fs.writeFile("./src/data/where.json", JSON.stringify(spreadsheetData), function(err) {
+          if (err) {
+              console.log(err);
+          }
+      });
+
+  });
+});
+
+
+
 module.exports = {
   entry : "./src/js/index.js",
   module: {
